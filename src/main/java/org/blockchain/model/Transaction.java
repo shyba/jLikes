15faed970940a5193ed5eab0133f418d04c52010
Victor Shyba <victor.shyba@gmail.com>
2024-09-05 23:40:12 -0300

    tx: immutable inputs and outputs

--- src/main/java/org/blockchain/model/Transaction.java
+++ src/main/java/org/blockchain/model/Transaction.java
@@ -7,12 +7,20 @@ import java.util.List;
 public class Transaction {
     public static final byte[] COINBASE = new byte[32];
     private static int version = 0;
-    private List<TransactionInput> inputs;
-    private List<TransactionOutput> outputs;
+    private final List<TransactionInput> inputs;
+    private final List<TransactionOutput> outputs;
 
     public Transaction(List<TransactionInput> inputs, List<TransactionOutput> outputs) {
         this.inputs = inputs;
         this.outputs = outputs;
+    }
+
+    public TransactionInput[] getInputs() {
+        return this.inputs.toArray(new TransactionInput[0]);
+    }
+
+    public TransactionOutput[] getOutputs() {
+        return this.outputs.toArray(new TransactionOutput[0]);
     }
 
     public boolean isCoinbase() {
